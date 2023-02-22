ae90d8f4275410f2c7b4709d961753e05a4ce68e
CodeMyst <code@myst.rs>
2023-02-22 10:14:09 +0100

    fix: removed test comments from the main file

--- lib/todorb.rb
+++ lib/todorb.rb
@@ -2,19 +2,6 @@
 
 # frozen_string_literal: true
 
-# TODO: This is a todo!
-
-# FIXME: This is a fixme!
-# A really long...
-# long...
-# comment
-
-# WARN: This is a warning!
-
-# TODO
-
-# TODO(#123): TODO with a ticket number attached!
-
 require 'English'
 require_relative 'file_utils'
 require_relative 'todo_checker'
